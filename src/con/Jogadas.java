c6f58e64b60839db65059975b728f35c84d90e22
IgorBavand <igor.bvn@gmail.com>
2019-05-01 18:26:49 -0300

    codigo completo

---
+++ src/con/Jogadas.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package con;
+
+/**
+ *
+ * @author igor
+ */
+public class Jogadas {
+    private String vez;
+
+    public String getVez() {
+        return vez;
+    }
+
+    public void setVez(String vez) {
+        this.vez = vez;
+    }
+
+   
+      
+}
---
+++ src/con/JogadoresI.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package con;
+
+/**
+ *
+ * @author igor
+ */
+public class JogadoresI {
+    private String jogador1;
+    private String jogador2;
+
+    public String getJogador1() {
+        return jogador1;
+    }
+
+    public void setJogador1(String jogador1) {
+        this.jogador1 = jogador1;
+    }
+
+    public String getJogador2() {
+        return jogador2;
+    }
+
+    public void setJogador2(String jogador2) {
+        this.jogador2 = jogador2;
+    }
+    
+    
+    
+}
